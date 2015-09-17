5ae663966e5d66b9f60ba127d26b8ca9a3da653e
reserad <reser13@gmail.com>
2015-09-17 16:59:42 -0400

    Original Commit

---
+++ Color Organizer/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Color_Organizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Upload = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Upload
+            // 
+            this.Upload.Location = new System.Drawing.Point(12, 404);
+            this.Upload.Name = "Upload";
+            this.Upload.Size = new System.Drawing.Size(632, 23);
+            this.Upload.TabIndex = 0;
+            this.Upload.Text = "Upload Image";
+            this.Upload.UseVisualStyleBackColor = true;
+            this.Upload.Click += new System.EventHandler(this.Upload_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(632, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(657, 439);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Upload);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Upload;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Color Organizer/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Organizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap newBitmap;
+        private void Upload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Image Files";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.DefaultExt = "jpg";
+            openFileDialog1.Filter = "JPEG (*.jpg)|*.jpg|PNG (*.png)|*.png";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String name = openFileDialog1.FileName;
+                Bitmap myBitmap = new Bitmap(name);
+                List<Color> colors = new List<Color>();
+                for(int x = 0; x < myBitmap.Width; x++)
+                {
+                    for (int y = 0; y < myBitmap.Height; y++)
+                    {
+                        colors.Add(myBitmap.GetPixel(x,y));
+                    }
+                }
+                colors.OrderBy(p => p.Name);
+                Dictionary<Color, int> counts = colors.GroupBy(b => b).ToDictionary(g => g.Key, g => g.Count());
+
+                newBitmap = new Bitmap(myBitmap.Width, myBitmap.Height);
+                int X = 0;
+                int currentPixel = 0;
+                int yLocation = 0;
+                foreach (var c in counts)
+                {
+                    int num = c.Value;
+                    Color _col = c.Key;
+
+                    if (yLocation == myBitmap.Height - 1) 
+                    {
+                        yLocation = 0;
+                        X++;
+                    }
+                    for (int count = 0; count < c.Value; count++) 
+                    {
+                        if (currentPixel % (myBitmap.Height - 1) == 0 && currentPixel != 0)
+                        {
+                            X++;
+                            yLocation = 0;
+                        }
+                        if (X < myBitmap.Width && yLocation < myBitmap.Height)
+                        {
+                            newBitmap.SetPixel(X, yLocation, _col);
+                        }
+                        yLocation++;
+                        currentPixel++;
+                    }
+                }
+                pictureBox1.Image = newBitmap;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (newBitmap != null)
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Title = "Save Image File";
+                dialog.CheckPathExists = true;
+                dialog.DefaultExt = "jpg";
+                dialog.Filter = "JPEG (*.jpg)|*.jpg|PNG (*.png)|*.png";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    newBitmap.Save(dialog.FileName + ".jpg", ImageFormat.Jpeg);
+                }
+            }
+        }
+    }
+}
